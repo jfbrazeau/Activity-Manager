1bb955e2134c6e7acce62f53e862097d9b38450f
jfbrazeau <jf.brazeau@yahoo.fr>
2013-06-26 21:49:30 +0200

    Added RCP product tycho resources

--- ui.web/plugins/org.activitymgr.ui.web.logic/src/org/activitymgr/ui/web/logic/AA.java
+++
@@ -1,8 +0,0 @@
-package org.activitymgr.ui.web.logic;
-
-public class AA {
-	public static void main(String[] args) {
-		System.out.println(System.getProperties().toString().replace(',', '\n')); 
-	}
-
-}
